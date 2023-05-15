1f76af1b9186a69abee0ac5389a8471a0cc040bf
LethargicHitman <69073059+LethargicHitman@users.noreply.github.com>
2023-05-15 22:04:00 +0530

    Add solution for s09, Add links to problems and re-format code (#5)
    Co-authored-by: NishanthSanjeevi <57482332+NishanthSanjeevi@users.noreply.github.com>

--- src/sln/s01.rs
+++ src/sln/s01.rs
@@ -1,45 +1,44 @@
 /**
-    1. Two Sum
+   1. Two Sum
+   Link: https://leetcode.com/problems/two-sum/
 
-    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+   Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+   You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-    You can return the answer in any order.
+   You can return the answer in any order.
 
-    
+   Example 1:
 
-    Example 1:
+   Input: nums = [2,7,11,15], target = 9
+   Output: [0,1]
+   Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+   Example 2:
 
-    Input: nums = [2,7,11,15], target = 9
-    Output: [0,1]
-    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-    Example 2:
+   Input: nums = [3,2,4], target = 6
+   Output: [1,2]
+   Example 3:
 
-    Input: nums = [3,2,4], target = 6
-    Output: [1,2]
-    Example 3:
+   Input: nums = [3,3], target = 6
+   Output: [0,1]
 
-    Input: nums = [3,3], target = 6
-    Output: [0,1]
-    
 
-    Constraints:
+   Constraints:
 
-    2 <= nums.length <= 104
-    -109 <= nums[i] <= 109
-    -109 <= target <= 109
-    Only one valid answer exists.
-    
+   2 <= nums.length <= 104
+   -109 <= nums[i] <= 109
+   -109 <= target <= 109
+   Only one valid answer exists.
 
-    Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
- */
+
+   Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+*/
 use std::collections::HashMap;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut hash: HashMap<i32, i32> = HashMap::new();
-        for index in 0 .. nums.len() {
+        for index in 0..nums.len() {
             let val: i32 = target - nums[index];
 
             match hash.get(&val) {
--- src/sln/s06.rs
+++ src/sln/s06.rs
@@ -1,49 +1,52 @@
 /**
-    6. Zigzag Conversion
+   6. Zigzag Conversion
+   Link: https://leetcode.com/problems/zigzag-conversion/
 
-    Medium
+   Medium
 
-    The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+   The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
 
-    P   A   H   N
-    A P L S I I G
-    Y   I   R
-    And then read line by line: "PAHNAPLSIIGYIR"
+   P   A   H   N
+   A P L S I I G
+   Y   I   R
+   And then read line by line: "PAHNAPLSIIGYIR"
 
-    Write the code that will take a string and make this conversion given a number of rows:
+   Write the code that will take a string and make this conversion given a number of rows:
 
-    string convert(string s, int numRows);
-    
+   string convert(string s, int numRows);
 
-    Example 1:
 
-    Input: s = "PAYPALISHIRING", numRows = 3
-    Output: "PAHNAPLSIIGYIR"
-    Example 2:
+   Example 1:
 
-    Input: s = "PAYPALISHIRING", numRows = 4
-    Output: "PINALSIGYAHRPI"
-    Explanation:
-    P     I    N
-    A   L S  I G
-    Y A   H R
-    P     I
-    Example 3:
+   Input: s = "PAYPALISHIRING", numRows = 3
+   Output: "PAHNAPLSIIGYIR"
 
-    Input: s = "A", numRows = 1
-    Output: "A"
-    
+   Example 2:
 
-    Constraints:
+   Input: s = "PAYPALISHIRING", numRows = 4
+   Output: "PINALSIGYAHRPI"
+   Explanation:
+   P     I    N
+   A   L S  I G
+   Y A   H R
+   P     I
 
-    1 <= s.length <= 1000
-    s consists of English letters (lower-case and upper-case), ',' and '.'.
-    1 <= numRows <= 1000
+   Example 3:
+
+   Input: s = "A", numRows = 1
+   Output: "A"
+
+
+   Constraints:
+
+   1 <= s.length <= 1000
+   s consists of English letters (lower-case and upper-case), ',' and '.'.
+   1 <= numRows <= 1000
 */
 impl Solution {
-    pub fn print2dvec (x: &Vec<Vec<u8>>) {
-        for i in 0 .. x.len() {
-            for j in 0 .. x[i].len() {
+    pub fn print2dvec(x: &Vec<Vec<u8>>) {
+        for i in 0..x.len() {
+            for j in 0..x[i].len() {
                 print!("{} ", x[i][j] as char);
             }
             println!("");
@@ -57,9 +60,9 @@ impl Solution {
 
         let mut x: Vec<Vec<u8>> = Vec::with_capacity(s.len() * num_rows as usize);
 
-        for _i in 0 .. num_rows {
+        for _i in 0..num_rows {
             let mut y: Vec<u8> = Vec::with_capacity(s.len());
-            for _j in 0 .. s.len() {
+            for _j in 0..s.len() {
                 y.push(0);
             }
             x.push(y);
@@ -84,7 +87,6 @@ impl Solution {
                     down = false;
                 }
             }
-
             // if we are going up zig-zag
             else {
                 if (row == 0) {
@@ -100,9 +102,8 @@ impl Solution {
 
         // Solution::print2dvec(&x);
 
-
-        for i in 0 .. x.len() {
-            for j in 0 .. x[i].len() {
+        for i in 0..x.len() {
+            for j in 0..x[i].len() {
                 if x[i][j] != 0 {
                     res.push(x[i][j]);
                 }
