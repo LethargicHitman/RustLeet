4295280d5d249d5fe7c5adf134739dccd47b2d2d
NishanthSanjeevi <57482332+NishanthSanjeevi@users.noreply.github.com>
2023-05-21 19:41:55 +0530

    Add solution for s16

--- src/sln/s15.rs
+++ src/sln/s15.rs
@@ -1,6 +1,8 @@
 /**
     15. 3Sum
-    https://leetcode.com/problems/3sum/
+    Link: https://leetcode.com/problems/3sum/
+
+    Medium
 
     Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
     Notice that the solution set must not contain duplicate triplets.
